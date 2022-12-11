9a262f88bf5b4946184dd6564c5e47d1411b4aac
Nam-Nguyen-Dev <nguyenn.dev@gmail.com>
2022-12-11 00:58:47 -0600

    capSentence

--- Beginner/capSentence/index-START.js
+++ Beginner/capSentence/index-START.js
@@ -8,9 +8,30 @@ return the equivalent of the sentence when capitalised. E.g
 
 
 function capSentence(text) {
-   // Code goes here
+   // For loop
+  /*  let newArr = []
+   text.split(" ")
+    .forEach(word => {
+      newArr.push(word[0].toUpperCase() + word.slice(1).toLowerCase())
+    })
+    
+   
+   return newArr.join(" ") */
+
+    //map
+   /*  return text.split(" ")
+    .map(element => element[0].toUpperCase() + element.slice(1).toLowerCase())
+    .join(" ") */
+
+    //replace
+    return text.toLowerCase().split(" ")
+    .map(word => {
+      return word.replace(word[0], word[0].toUpperCase())
+    })
+    .join(" ")
+   
 }
 
-
+capSentence('cap this')
 
 module.exports = capSentence
