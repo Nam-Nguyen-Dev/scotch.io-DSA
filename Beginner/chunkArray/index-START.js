8eccb45ab9e2d77ebd9f60a651ce9d12021d8cf9
Nam-Nguyen-Dev <nguyenn.dev@gmail.com>
2022-12-12 01:48:41 -0600

    chunkArray

--- Beginner/chunkArray/index-START.js
+++ Beginner/chunkArray/index-START.js
@@ -5,8 +5,46 @@
 */
 
 function chunkArray(array, size) {
-    // Code goes here
-}
+    /* let newArr = []
+    for (let i = 0; i < array.length; i=i+size){
+        newArr.push(array.slice(i,i+size))
+    }
+    return newArr */
 
+    //Using for...of loop
+    /* let result = []
+    for (const value of array){
+        let lastArray = result[result.length -1]
+        if(!lastArray || lastArray.length === size){
+            console.log(`lastArray: ${lastArray}`)
+            console.log(`result: ${result}`)
+            result.push([value])
+            console.log(`lastArray: ${lastArray}`)
+            console.log(`result: ${result}`)
+        } else {
+            console.log(`lastArray: ${lastArray}`)
+            console.log(`result: ${result}`)
+            lastArray.push(value)
+            console.log(`lastArray: ${lastArray}`)
+            console.log(`result: ${result}`)
+        }
+    }
+    return result */
+
+    //Using splice()
+   /*  let result = []
+    let arrayCopy = [...array]
+    while(arrayCopy.length > 0){
+        result.push(arrayCopy.splice(0,size))
+    }
+    return result */
+
+    //Using recursion
+    if (array.length <= size){
+        return [array]
+    }
+    return [array.slice(0,size), ...chunkArray(array.slice(size), size)]
+}
+console.log(chunkArray([1,2,3,4,5,6], 2))
 
 module.exports = chunkArray
