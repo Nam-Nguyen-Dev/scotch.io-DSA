92ac8ead55747995ee2ac527e77ddf1a5825b9e3
Nam-Nguyen-Dev <nguyenn.dev@gmail.com>
2022-12-11 19:53:10 -0600

    Search and Replace

---
+++ Beginner/searchReplace/index-START.js
@@ -0,0 +1,13 @@
+function searchReplace (str, word, newWord) {
+    /* if (word[0] === word[0].toUpperCase()){
+        newWord = newWord[0].toUpperCase() + newWord.slice(1)
+    }
+    return str.replace(word, newWord) */
+
+    //Using regex
+    let regex = new RegExp(word, "gi")
+    if (/[A-Z]/.test(word[0])){
+        newWord = newWord[0].toUpperCase() + newWord.slice(1)
+    }
+    return str.replace(regex, newWord)
+}
